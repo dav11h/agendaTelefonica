3a03bb6419636994e45a5a52c74dd4a0a9efccb7
Davih Dias de Castro dos Santos <davihdias27@gmail.com>
2023-10-02 13:15:57 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let tb = document.querySelector('tbody')
+let inpTel = document.querySelector('#tel')
+let inpText = document.querySelector('#nome')
+let button = document.querySelector('#button')
+
+button.addEventListener('click', function(e){
+    e.preventDefault()
+
+    adicionar()
+})
+
+function adicionar(){
+    if(inpTel.value.length >= 6 && inpTel.value.length <= 15 && inpText.value.length >= 4){
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('td')
+        let td2 = document.createElement('td')
+
+        td1.innerHTML = `${inpText.value}`
+        td2.innerHTML = `${inpTel.value}`    
+        
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+
+        tb.appendChild(tr)
+    } else{
+        alert('Dados incompletos, ou errados digite novamente')
+        console.error('Dados errados ou incompletos')
+    }
+}
